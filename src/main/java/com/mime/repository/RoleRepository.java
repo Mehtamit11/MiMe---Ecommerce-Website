afa35938d42730a0f0cd186c46324730c4b6907e
MitMehta <mitmehta721@gmail.com>
2026-04-22 19:39:22 +0530

    Add RoleRepository interface for role management

---
+++ src/main/java/com/mime/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.mime.repository;
+
+import com.mime.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
