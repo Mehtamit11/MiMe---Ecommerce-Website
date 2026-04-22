dca55eed7098713f7f4ee66bd3c7502697c19c68
MitMehta <mitmehta721@gmail.com>
2026-04-22 20:08:44 +0530

    Add ProductService interface for product management

---
+++ src/main/java/com/mime/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.mime.service;
+
+import com.mime.model.Product;
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+
+    Product getProductById(Long id);
+
+    Product saveProduct(Product product);
+
+    void deleteProduct(Long id);
+}
