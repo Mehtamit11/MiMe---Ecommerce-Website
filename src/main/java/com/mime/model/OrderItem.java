ba1a3bdffbc9d350138fef839f781fa805652d49
MitMehta <mitmehta721@gmail.com>
2026-04-22 19:36:55 +0530

    Add OrderItem entity with JPA annotations

---
+++ src/main/java/com/mime/model/OrderItem.java
@@ -0,0 +1,24 @@
+package com.mime.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+
+    private double price;
+
+    @ManyToOne
+    private Order order;
+}
