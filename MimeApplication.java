f3abee17d65fe69e378bc6f546b38e648a6f48b2
MitMehta <mitmehta721@gmail.com>
2026-04-22 19:26:25 +0530

    Initialize Spring Boot application in MimeApplication

---
+++ MimeApplication.java
@@ -0,0 +1,12 @@
+package com.mime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimeApplication.class, args);
+    }
+}
