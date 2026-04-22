b7f05e1d38d483d0b773b0cbcac4650e715643f2
MitMehta <mitmehta721@gmail.com>
2026-04-22 20:09:20 +0530

    Create ProductServiceImpl.java

---
+++ src/main/java/com/mime/service/impl/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.mime.service.impl;
+
+import com.mime.model.Product;
+import com.mime.repository.ProductRepository;
+import com.mime.service.ProductService;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository repo;
+
+    public ProductServiceImpl(ProductRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    public Product saveProduct(Product product) {
+        return repo.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        repo.deleteById(id);
+    }
+}
