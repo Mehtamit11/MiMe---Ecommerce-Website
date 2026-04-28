3596320cff198791906dc97cbc2e1035622c0538
MitMehta <mitmehta721@gmail.com>
2026-04-28 22:21:50 +0530

    Add methods to UserService interface

--- src/main/java/com/mime/service/UserService.java
+++ src/main/java/com/mime/service/UserService.java
@@ -2,6 +2,12 @@ package com.mime.service;
 
 import com.mime.model.User;
 
+import java.util.List;
+
 public interface UserService {
     User registerUser(User user);
+
+    User createUser(User user, boolean adminRole);
+
+    List<User> getAllUsers();
 }
